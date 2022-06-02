bd7fb0d619b7057c6bb4e47e2930d8666e0c70bf
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-06-02 12:51:06 -0700

    testing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+from datetime import datetime
 
 app_Id = "e8eca5ad"
 API_key = "3a6a6798d25ea61f3512af51af8376de"
@@ -27,5 +27,32 @@ params = {
 endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 response = requests.post(url=endpoint, json=params, headers=headers)
+data = response.json()
+print(data)
 
-print(response.json())
+exercise = (data['exercises'][0]['name'])
+
+Duration = (data['exercises'][0]['duration_min'])
+
+Calories = (data['exercises'][0]['nf_calories'])
+
+date_now = datetime.now().strftime("%d%m%Y")
+time_now = datetime.now().strftime("%X")
+
+
+
+sheety_data = "https://api.sheety.co/1b804a7375398b54f22f199adbfa614d/copyOfMyWorkouts/workouts"
+
+params = {
+    "workout":{
+        "date": date_now,
+        "time": time_now,
+        "excercise": exercise,
+        "duration": Duration,
+        "calories": Calories
+    }
+}
+
+response_1 = requests.post(url= sheety_data, json= params)
+
+print(response_1.json())
